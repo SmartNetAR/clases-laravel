0b1cf8579af7e2d869253c8cdd5802b5b9dacf88
Omar Garcia <leonardo@smartnet.com.ar>
2019-11-13 16:29:10 -0300

    final project created

---
+++ final/api-event-manager/app/Http/Controllers/UserController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Validator;
+use App\User;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    { 
+        $validator = Validator::make($request->all(), [ //creamos la validación
+            'name' => 'required', 
+            'email' => 'required|email',
+            // 'country' => 'required',
+            // 'state' => 'required',
+            // 'city' => 'required',
+            'password' => 'required', 
+            'c_password' => 'required|same:password', 
+        ]);
+        if ($validator->fails()) {//validamos
+            return response()->json(['error'=>$validator->errors()], 401);
+        }
+        
+        //creamos el usuario
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+    
+        //creamos el token y se lo enviamos al usuario
+        $success['token'] =  $user->createToken('MyApp')->accessToken;
+        return response()->json(['success'=>$success], 200);
+    }
+}
