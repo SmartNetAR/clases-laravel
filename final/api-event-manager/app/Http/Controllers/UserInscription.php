52152ea8d963d441be6bc23e9e960641eb3f2db9
Your Name <you@example.com>
2019-11-20 19:33:36 -0300

    inscription

--- final/api-event-manager/app/Http/Controllers/UserInscription.php
+++ final/api-event-manager/app/Http/Controllers/UserInscription.php
@@ -3,8 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Validator;
+use Auth;
+use App\User;
+
 
 class user_inscription extends Controller
+
 {
-    //
+    public function inscription(Request $request)
+    { 
+        $validator = Validator::make($request->all(), [ //creamos la validación
+            'name' => 'required', 
+            'email' => 'required|email',
+             'country' => 'required',
+             'state' => 'required',
+             'city' => 'required',
+            'password' => 'required', 
+            'c_password' => 'required|same:password', 
+        ]);
+        if ($validator->fails()) {//validamos
+            return response()->json(['error'=>$validator->errors()], 401);
+        }
+        
+        //creamos el usuario
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+    
+        //creamos el token y se lo enviamos al usuario
+        $success['token'] =  $user->createToken('MyApp')->accessToken;
+        return response()->json(['success'=>$success], 200);
+    }
+
+    public function profile() {
+        $user = Auth::user(); 
+        return response()->json(['success' => $user], 200); 
+    }
 }
